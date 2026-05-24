f8a54a8158a43ed8616e23ea5016fe30e17b30ea
Felipe Medina <felipe.medina98@gmail.com>
2026-05-24 00:11:28 -0400

    chore: migrate project from Cloudflare Pages to Cloudflare Workers

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import devServer from '@hono/vite-dev-server'
-import pages from '@hono/vite-cloudflare-pages'
+import build from '@hono/vite-build/cloudflare-workers'
 
 export default defineConfig({
   plugins: [
-    pages(),
+    build(),
     devServer({
       entry: 'src/index.tsx'
     })
